172dd5554508a95b8ded268bb29a638bd0beac0f
zifeng <zifeng.1024@gmail.com>
2019-01-03 14:44:34 +0800

    dep init

--- pkg/gland/start.go
+++ pkg/gland/start.go
@@ -1,1 +1,37 @@
-package gland
+package main
+
+import (
+	"net"
+
+	api "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v2"
+	"github.com/envoyproxy/go-control-plane/pkg/cache"
+	xds "github.com/envoyproxy/go-control-plane/pkg/server"
+	"google.golang.org/grpc"
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+)
+
+type hash struct {
+}
+
+func (hash hash) ID(node *core.Node) string {
+	return node.Id
+}
+
+func main() {
+	snapshotCache := cache.NewSnapshotCache(false, hash{}, nil)
+	server := xds.NewServer(snapshotCache, nil)
+	grpcServer := grpc.NewServer()
+	lis, _ := net.Listen("tcp", ":8080")
+
+	discovery.RegisterAggregatedDiscoveryServiceServer(grpcServer, server)
+	api.RegisterEndpointDiscoveryServiceServer(grpcServer, server)
+	api.RegisterClusterDiscoveryServiceServer(grpcServer, server)
+	api.RegisterRouteDiscoveryServiceServer(grpcServer, server)
+	api.RegisterListenerDiscoveryServiceServer(grpcServer, server)
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			// error handling
+		}
+	}()
+}
